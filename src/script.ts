3a3f8317c22c43899033e92040f01a74a28407db
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-01-27 15:47:10 +0300

    catch problem

--- src/script.ts
+++ src/script.ts
@@ -1,20 +1,60 @@
-import { ApiPromise, WsProvider } from '@polkadot/api'
+import { ApiPromise, WsProvider, Keyring } from '@polkadot/api'
+import { options } from '@acala-network/api'
+
+const PHRASE = 'time copper course profit all pledge priority illegal please couch nest glance'
+const Receiver = '5ED8PaU5m2gQoaASMzrF7JrcJMfj4yZMBPaUY84VjozyTo7Y'
+const url = 'wss://mandala.polkawallet.io'
 
 async function main() {
-    // Initialise the provider to connect to the local node
-    const provider = new WsProvider('wss://rpc.polkadot.io');
 
-    // Create the API and wait until ready
-    const api = await ApiPromise.create({ provider });
+    let api = await connectionCreator(url)
 
-    // Retrieve the chain & node information information via rpc calls
-    const [chain, nodeName, nodeVersion] = await Promise.all([
-        api.rpc.system.chain(),
-        api.rpc.system.name(),
-        api.rpc.system.version()
-    ]);
+    const keyring = new Keyring({ type: 'sr25519' })
+    const newPair = keyring.addFromUri(PHRASE)
 
-    console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+    const balance_before_transfer = await print_current_balance(api, newPair)
+
+    //make a transfer
+    await api.tx.currencies.transfer(Receiver, {'token': 'DOT'}, 100000000).signAndSend(newPair)
+
+    await api.disconnect()
+    api = await connectionCreator(url)
+
+    api.query.tokens.accounts(newPair.address, {'token': 'DOT'}, ({ free }) => {
+        console.log(`Balance from subscripttion is ${free}`)
+        if (balance_before_transfer.toString() != free.toString()){
+            throw new Error("Try again, it's happening 1/5 tymes")
+        }
+    });
+
+    const interval = setInterval(doStuff, 1000)
+
+    await delay(60000)
+    console.log(`I can't fetch balance by subscription, let's reconnect and do it again...`)
+    clearInterval(interval)
+    await api.disconnect()
+    api = await connectionCreator(url)
+    await print_current_balance(api, newPair)
+
+    return
 }
 
-main().catch(console.error).finally(() => process.exit());
+async function connectionCreator(url: string): Promise<ApiPromise> {
+    const provider = new WsProvider(url)
+    return ApiPromise.create(options({provider}))
+}
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const print_current_balance = (api, keyPair) => new Promise(async resolve => {
+    await api.query.tokens.accounts(keyPair.address, {'token': 'DOT'}, ({free}) => {
+        console.log(`Curent balance is ${free}`)
+        resolve(free)
+    })
+})
+
+function doStuff() {
+    console.count('Seconds after subscription')
+ }
+
+main().catch(console.error).finally()
